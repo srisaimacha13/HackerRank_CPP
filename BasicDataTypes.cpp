9075b8cfe1add252b3792d22eabad6b930879413
Macha <srisaimacha13>
2017-04-03 12:20:36 +0530

    basics

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char ch;
+    int a,b;
+    long long int c;
+    double d,e;
+    scanf("%d %d %lld %c %lf %lf",&a,&b,&c,&ch,&d,&e);
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%lld\n",c);
+    printf("%c\n",ch);
+    printf("%lf\n",d);
+    printf("%lf\n",e);
+    return 0;
+}
+
---
+++ Functions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int max_of_four(int a,int b,int c,int d){
+	
+	int max =a;
+	if(b>a && b>c && b>d){
+		max = b;
+	}else if(c>a && c>b && c>d){
+		max=c;
+	}else if(d>a && d>b && d>c){
+		max=d;
+	}
+	else{
+		max=a;
+	}
+	return max ;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
